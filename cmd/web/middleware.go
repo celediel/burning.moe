177556399dde3ceee90a33abf82c3ad3ca475997
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2024-01-24 23:43:27 -0800

    more middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -8,6 +8,10 @@ import (
 
 // Middleware is a slice of Middleware (aka func(n http.Handler) http.Handler {})
 var Middleware []func(next http.Handler) http.Handler = []func(next http.Handler) http.Handler{
+	// chi's recommended list
+	middleware.RequestID,
+	middleware.RealIP,
+	// plus custom request logger
 	func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			app.Logger.Info("REQUEST", "url", r.URL, "ip", r.RemoteAddr, "useragent", r.UserAgent())
